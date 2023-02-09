b2a9d597072ce09ad78fffef71eff7837dd9a579
ben-m-m <mainaben23@gmail.com>
2023-02-09 16:12:54 +0000

    base

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,11 @@
 """class BaseModel defines all common attributes/methods for other classes"""
 
 
+import uuid
+import models
+from datetime import datetime
+
+
 class BaseModel:
     """BaseModel class"""
     def __init__(self, *args, **kwargs):
@@ -14,8 +19,8 @@ class BaseModel:
         if kwargs != {}:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
-                    val = datetime.strptime(value "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, k, v)
+                    val = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, k, val)
                     continue
                 if k != "__class__":
                     setattr(self, k, v)
@@ -29,3 +34,22 @@ class BaseModel:
         """returns string representation of the instance"""
         st = "[{:s}] ({:s}) {}"
         return st.format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        updates public instance attribute updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all
+        keys/values of __dict__ of the instance
+        """
+        new = dict(self.__dict__)
+        new["__class__"] = type(self).__name__
+        new["created_at"] = new["created_at"].isoformat()
+        new["updated_at"] = new["updated_at"].isoformat()
+
+        return new
